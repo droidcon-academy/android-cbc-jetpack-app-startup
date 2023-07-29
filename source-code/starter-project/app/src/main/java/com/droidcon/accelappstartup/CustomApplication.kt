42c7028bf3a227be17e769a241ca1a03ae22cdb4
John Li <johnliglobal@gmail.com>
2023-07-29 08:42:26 -0700

    Created starter project

---
+++ source-code/starter-project/app/src/main/java/com/droidcon/accelappstartup/CustomApplication.kt
@@ -0,0 +1,11 @@
+package com.droidcon.accelappstartup
+
+import android.app.Application
+
+class CustomApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // startup code
+    }
+}
