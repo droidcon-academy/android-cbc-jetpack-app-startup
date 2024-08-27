5ad96bece7431d9d6358318eefb188bf9186d56b
Musab Nasredlin <musabagab2010@gmail.com>
2024-08-28 01:17:05 +0400

    Updated starter project gradle and agp, added databaseiniiltizer to the helper files

---
+++ helper-files/initializer-files/DatabaseInitializer.kt
@@ -0,0 +1,15 @@
+package com.droidcon.accelappstartup.startup.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.droidcon.accelappstartup.startup.dependency.FakeDatabase
+
+class DatabaseInitializer: Initializer<FakeDatabase> {
+    override fun create(context: Context): FakeDatabase {
+        return FakeDatabase()
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
